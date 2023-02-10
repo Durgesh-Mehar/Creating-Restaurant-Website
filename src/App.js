ff8c889ce4d64b63e0fa51a9c59f45cc378dea24
Durgesh Mehar <durgeshmehar1380@gmail.com>
2023-02-10 08:16:36 -0800

    Showing and Enabling the delete of Cart Items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Fragment, useState} from 'react';
-
+import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
-
+//console.log(cartIsShown)
   const showCartHandler = () => {
     setCartIsShown(true);
   };
@@ -16,11 +16,11 @@ const App = () => {
   };
 
   return (
-    <Fragment>
+    <CartProvider >
      {cartIsShown && <Cart onClose={hideCartHandler}/>} 
       <Header onShowCart={setCartIsShown}/>
       <Meals />
-    </Fragment>
+    </CartProvider>
   )
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import CartContext from '../../store/cart-context'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItem = cartCtx.items.reduce((curNumbar,item) => {
+    return curNumbar + item.amount;
+  },0)
   return (
    <button className={classes.button} onClick={props.onClick}>
     <samp className={classes.icon}>
         <CartIcon />
     </samp>
     <samp>Your Cart</samp>
-    <samp className={classes.badge}>3</samp>
+    <samp className={classes.badge}>{numberOfCartItem}</samp>
    </button>
   )
 }
